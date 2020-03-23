305e8920265a6e6786ec41e2977fca8c4005d93c
Marlon Gayle <mgayle@realdecoy.com>
2020-03-22 21:27:25 -0500

    Add and Configure Swagger middleware

--- LifeBank.Api/Startup.cs
+++ LifeBank.Api/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace LifeBank.Api
 {
@@ -26,6 +20,14 @@ namespace LifeBank.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplicationInsightsTelemetry();
+
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "LifeBank API", Version = "v1" });
+            });
+
+            services.AddSwaggerGenNewtonsoftSupport();
+
             services.AddControllers();
         }
 
@@ -37,10 +39,20 @@ namespace LifeBank.Api
                 app.UseDeveloperExceptionPage();
             }
 
+            // Enable Middelware to serve generated Swager as JSON endpoint
+            app.UseSwagger();
+
+            // Enable Middelware to serve Swagger UI (HTML, JavaScript, CSS etc.)
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "LifeBank API");
+            });
+
+            // Https Redirect middleware
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            //Authorization middleware
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
