d69310760d1c6ee4a83ef13d256bf8cc69bd15fe
Marlon Gayle <mgayle@realdecoy.com>
2020-03-25 15:02:22 -0500

    Created API route utility class

---
+++ LifeBank.Api/Routes/ApiRoutes.cs
@@ -0,0 +1,11 @@
+﻿namespace LifeBank.Api.Routes
+{
+    public static class ApiRoutes
+    {
+        public const string Domain = "api";
+        public const string Verion = "v1";
+        public const string Base = "${api}/${Version}";
+
+
+    }
+}
