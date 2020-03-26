7b64a00d53ec1a8a450cea43760bcc96d45ec0c3
Marlon Gayle <mgayle@realdecoy.com>
2020-03-26 17:00:02 -0500

    Create Test project folder structure and implemented unit test for Health Check.

---
+++ LifeBank.UnitTests/Application/ApplicationHealthCheckTests.cs
@@ -0,0 +1,35 @@
+﻿using LifeBank.Api;
+using LifeBank.Application.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using System.Net.Http;
+using Xunit;
+
+namespace LifeBank.UnitTests.Application
+{
+    public class ApplicationHealthCheckTests
+    {
+        private readonly TestServer Server;
+        private readonly HttpClient Client;
+
+        public ApplicationHealthCheckTests()
+        {
+            Server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+
+            Client = Server.CreateClient();
+        }
+
+        [Fact]
+        public void ApplicationHealthCheck()
+        {
+            var response = Client.GetAsync("/health");
+
+            var result =  response.Result.Content.ReadAsStringAsync();
+            var healthCheckResponse = JsonConvert.DeserializeObject<HealthCheckResponse>(result.Result);
+
+            Assert.Equal("Healthy", healthCheckResponse.Status);
+        }
+    }
+}
