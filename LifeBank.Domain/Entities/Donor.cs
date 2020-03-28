f029163907713947246234de80a8b73f25751680
Marlon Gayle <mgayle@realdecoy.com>
2020-03-27 19:50:31 -0500

    Created Donor entity

---
+++ LifeBank.Domain/Entities/Donor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LifeBank.Domain.Entities
+{
+    public class Donor
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleInital { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string BloodType { get; set; }
+    }
+}
